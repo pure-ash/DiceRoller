0fb7bbf6aa1bbcf8e8f4b5c1da61e3c665501e04
pure-ash <pureash2023@outlook.com>
2023-04-21 19:49:53 +0700

    Adding pictureView.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,38 +2,55 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 
-/**
- * This Activity show button that would show random
- * dice number when button pressed*/
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {        //onCreate > main > later learned on project
-        super.onCreate(savedInstanceState)                      //onCreate > main > later learned on project
-        setContentView(R.layout.activity_main)                  //onCreate > main > later learned on project
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        // Displaying a button that execute roll dice when clicked
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
-    }
-    /**
-     * This rollDice func. taking Dice class and apply roll
-     * method and express it in text view*/
-    private fun rollDice() {
-        val dice = Dice(6)
-        val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
 
-        val diceTwo = Dice(6)
-        val diceRollTwo = diceTwo.roll()
-        val resultTextViewTwo: TextView = findViewById(R.id.textViewTwo)
-        resultTextViewTwo.text = diceRollTwo.toString()
+        // Dice pre rolled when user execute the app
+        rollDice()
+    }
+    // Function that roll dice
+    private fun rollDice() {
+        // Creating an object dice from Dice class
+        val dice = Dice(6)
+
+        // Saving the value of roll method
+        val diceRoll = dice.roll()
+
+        // Saving the imageView id to diceImage object
+        val diceImage: ImageView =  findViewById(R.id.imageView)
+
+        // Saving the right dice picture based on the dice roll number
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Setting the image view with the right saved image on drawableResource object
+        diceImage.setImageResource(drawableResource)
+
+        // Setting the image description to number that been rolled
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
+// Class that simulate a dice where taking parameters as sides of the dice
 class Dice(private val numSides: Int) {
+
+    // Method that pick random sides, simulating a roll of a dice
     fun roll(): Int {
         return (1..numSides).random()
     }
